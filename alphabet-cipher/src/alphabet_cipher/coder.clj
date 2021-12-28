5bf1e8fc35517aa97c0374e836d502ab3aec51b0
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-12-28 12:17:37 +0000

    aphabet cipher 2/3

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,40 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :refer [index-of]]))
 
-(defn encode [keyword message]
-  "encodeme")
+(def ab "abcdefghijklmnopqrstuvwxyz")
 
-(defn decode [keyword message]
-  "decodeme")
+(def alphabet
+  (into {} (map-indexed vector ab)))
+
+(defn encode [secret-key message]
+  (apply str
+         (map
+          (fn [k m]
+            (alphabet (mod (+ (index-of ab k) (index-of ab m)) 26)))
+          (take (count message) (cycle secret-key))
+          message)))
+
+(defn decode [secret-key message]
+  (apply str
+         (map
+          (fn [m k] (alphabet (mod (- (index-of ab m) (index-of ab k)) 26)))
+          message
+          (take (count message) (cycle secret-key)))))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [extended-cipher (decode cipher message)]
+    extended-cipher))
 
+(decipher "meetmebythetree" "egsgqwtahuiljgs")
+
+(comment 
+(alphabet (mod (- (index-of ab \e) (index-of ab \s)) 26))
+(alphabet (mod (- (index-of ab \g) (index-of ab \e)) 26))
+
+(def message "meetmebythetree")
+(def msg "egsgqwtahuiljgs")
+(count message)
+(apply str (take (count msg) (cycle "scones")))
+
+
+)
